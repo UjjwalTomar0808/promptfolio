e0d072a390bf258e66221b47cda9b65b675d2784
ujjwal <ujjwaltomar.0808@gmail.com>
2025-05-22 20:08:16 +0530

    Add config.jsx

---
+++ src/config.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+
+export const github_username = 'UjjwalTomar0808'
+export const discord_usertag = 'ujjwaltomar_0808'
+export const discord_userid = '1185916051293216858'
+export const email = 'ujjwaltomar.0808@gmail.com'
+
+export const projects = [
+  'UjjwalTomar0808/AR-Shopsy',
+  'UjjwalTomar0808/Auth',
+  'UjjwalTomar0808/chat-backend',
+  'UjjwalTomar0808/Portfolio',
+  'UjjwalTomar0808/ICETCT-25',
+  'UjjwalTomar0808/shader',
+]
+
+export const projectData = [
+  {
+    name: 'AR-Shopsy',
+    html_url: 'https://github.com/UjjwalTomar0808/AR-Shopsy',
+    description: 'Augmented Reality shopping platform.',
+  },
+  {
+    name: 'Auth',
+    html_url: 'https://github.com/UjjwalTomar0808/Auth',
+    description: 'Authentication system with JWT and bcrypt.',
+  },
+  {
+    name: 'chat-backend',
+    html_url: 'https://github.com/UjjwalTomar0808/chat-backend',
+    description: 'Backend APIs for a real-time chat app.',
+  },
+  {
+    name: 'Portfolio',
+    html_url: 'https://github.com/UjjwalTomar0808/Portfolio',
+    description: 'Personal portfolio website built with React and Vite.',
+  },
+  {
+    name: 'ICETCT-25',
+    html_url: 'https://github.com/UjjwalTomar0808/ICETCT-25',
+    description: 'React + Vite template with ESLint and Tailwind CSS setup.',
+  },
+  {
+    name: 'shader',
+    html_url: 'https://github.com/UjjwalTomar0808/shader',
+    description: 'Shader programming experiments and demos.',
+  },
+];
+
+
+export const links = [
+  {
+    name: 'GitHub',
+    icon: 'fab fa-fw fa-github',
+    link: 'https://github.com/UjjwalTomar0808',
+    description: 'My profile on GitHub',
+  },
+  {
+    name: 'Website',
+    icon: 'fas fa-fw fa-coffee',
+    link: 'https://portfolio-ujjwal-tomar.vercel.app/',
+    description: 'Personal website',
+  },
+  {
+    name: 'Discord',
+    link: `https://discord.com/users/${discord_userid}`,
+    icon: 'fab fa-fw fa-discord',
+    description: `${discord_usertag} | Add me as friend!`,
+  },
+]
+
+export const info = (
+  <>
+    <p>Yo! I'm a highschooler who codes as a hobby.</p>
+    <p>
+      I've got intermediate experience with{' '}
+      <span className="highlighted">JavaScript</span> and{' '}
+      <span className="highlighted">Node.js</span>.
+    </p>
+    <p>
+      I've been doing{' '}
+      <span className="highlighted">front-end web designing</span> as a
+      fun pastime for a couple of years now. Currently, I'm learning{' '}
+      <span className="highlighted">React</span> and{' '}
+      <span className="highlighted">Next.js</span>
+    </p>
+    <p>
+      Aside from that, I have basic knowledge of{' '}
+      <span className="highlighted">python</span>,{' '}
+      <span className="highlighted">java</span> and{' '}
+      <span className="highlighted">C++</span>
+    </p>
+  </>
+)
